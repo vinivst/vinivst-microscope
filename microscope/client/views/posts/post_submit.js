0997785a903562cbd118b4751440cc49fbd6a30f
vinivst <vinicius.santiago@gmail.com>
2016-01-28 17:07:37 -0200

    Teste
    Teste

---
+++ microscope/client/views/posts/post_submit.js
@@ -0,0 +1,27 @@
+Template.postSubmit.events({
+
+	'submit form': function(e) {
+
+		e.preventDefault();
+
+		var post = {
+
+			url: $(e.target).find('[name=url]').val(),
+
+			title: $(e.target).find('[name=title]').val(),
+
+		}
+
+		Meteor.call('post', post, function(error, id) {
+			if (error) {
+				return alert(error.reason);
+			}
+
+			Router.go('postPage', {_id: id});
+		});
+
+		
+
+	}
+
+});
---
+++ microscope/lib/permissions.js
@@ -0,0 +1,7 @@
+// check that the userId specified owns the documents
+
+ownsDocument = function(userId, doc) {
+
+	return doc && doc.userId === userId;
+
+}
