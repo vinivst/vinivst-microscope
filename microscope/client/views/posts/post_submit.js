9c218bc87bfb29800381ac59045a7a019c4803cb
vinivst <vinicius.santiago@gmail.com>
2016-01-29 17:03:04 -0200

    Atualização Tratamento de Errors
    Feito tratamento de erros

--- microscope/client/views/posts/post_submit.js
+++ microscope/client/views/posts/post_submit.js
@@ -14,10 +14,13 @@ Template.postSubmit.events({
 
 		Meteor.call('post', post, function(error, id) {
 			if (error) {
-				return alert(error.reason);
+				throwError(error.reason);
+				if (error.error === 302) {
+					Router.go('postPage', {_id: error.details});
+				} else {
+					Router.go('postPage', {_id: id});
+				}
 			}
-
-			Router.go('postPage', {_id: id});
 		});
 
 		
